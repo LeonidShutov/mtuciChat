3eb2ddb2037b27f0aa982cea8dac56c31a5e047d
Leonid Shutov <leonid.shutov@gmail.com>
2018-02-04 22:55:57 +0300

    implement login class

---
+++ app/src/main/java/com/leonidshutov/androidchat/UserDetails.java
@@ -0,0 +1,11 @@
+package com.leonidshutov.androidchat;
+
+/**
+ * Created by shiz on 04.02.2018.
+ */
+
+class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
