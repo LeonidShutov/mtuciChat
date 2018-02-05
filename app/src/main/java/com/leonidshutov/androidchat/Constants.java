70970c504fc316a9ed805fc3109ffed0771ac620
Leonid Shutov <leonid.shutov@gmail.com>
2018-02-05 15:03:01 +0300

    implement registration and chat

--- app/src/main/java/com/leonidshutov/androidchat/Constants.java
+++ app/src/main/java/com/leonidshutov/androidchat/Constants.java
@@ -2,8 +2,12 @@ package com.leonidshutov.androidchat;
 
 /**
  * Created by shiz on 04.02.2018.
+ * Constants
  */
 
-class Constants {
-    public static final String FIRE_BASE_URL = "https://mtucichat.firebaseio.com/";
+interface Constants {
+    String FIRE_BASE_URL = "https://mtucichat.firebaseio.com/";
+    String BLANK_ERROR = "can't be blank";
+    String SHORT_PASS_ERROR = "at least 5 characters long";
+    String REGISTRATION_SUCCESSFUL = "registration successful";
 }
